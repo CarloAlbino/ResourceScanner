e8ae6eb317fa9ed921b5cca5cac7bdc34e1af799
CarloAlbino <carloalbino@gmail.com>
2017-01-13 12:01:29 -0500

    Project with test scene + updated gitignore

---
+++ ResourceScanner/Assets/Scripts/TestScripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    [SerializeField]
+    private float m_rotateSpeed = 1.0f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        // Rotate about the y axis
+        transform.Rotate(Vector3.up, m_rotateSpeed * Time.deltaTime);
+	}
+}
